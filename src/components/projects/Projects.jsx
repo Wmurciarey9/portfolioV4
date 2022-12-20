4eefcbf8d5bbd38b01baf67f6b7a689c12ffcd89
Wilson Murcia Rey <wmurciarey9@gmail.com>
2022-12-19 22:24:57 -0500

    added link for netflix project.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -9,20 +9,20 @@ export const Projects = () => {
       <Project
         title="Netflix Clone"
         img="https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&w=1000&q=80"
-        liveUrl=""
-        gitHub=""
-        description="A booking app for business time management. Set appointments and control your work time more efficienly"
+        liveUrl="https://wilsonmrey-netflix-clone.netlify.app/"
+        gitHub="https://github.com/Wmurciarey9/clone-netflix-react"
+        description="Cloned latest version UI of netflix. Added a navigation overlay to access all available pages. (Login, Home, Profiles, Watch) This is only the front end site. I have a full stack version with it's own database. Login and register process working. Also an admin exclusive dashboard to manage. Only issue is that there is no movies inside the database. "
         techOne="React"
         techTwo="Saas"
-        techThree="MongoDB"
-        techFour="Node.js"
+        techThree="HTML"
+        techFour="CSS"
       />
       <Project
-        title="Eccommerce Clone"
+        title="Eccommerce Store"
         img="https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bmV0ZmxpeHxlbnwwfHwwfHw%3D&w=1000&q=80"
         liveUrl=""
         gitHub=""
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+        description="****IN PROGRESS**** Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
       been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
       galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text
       of the printing and typesetting industry."
